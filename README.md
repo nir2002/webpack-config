a784b98fc9d62e6e052f77c2076a23715100efdd
Nir Parisian <nir.parisian@gmail.com>
2018-04-28 14:36:13 +0300

    added babel-core, babel-loader, babel-preset-end and babel-preset-stage-0. also put use the html-webpack plugin

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # webpack-config
 a set of webpack configuration files
+
+- Babel - 
+ * babel-core: Transform ES6 to ES5
+ * babel-loader: Webpack helper that transform JavaScript dependencies 
+ * babel-preset-env: Determines which transformations/plugins to use and polyfills
+ * babel-preset-stage-0: Evolving ECMAScript
+ 
+ - Plugins -
+  * HTML-webpack-plugin: generates an HTML file with <script> injected and minifies the file
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,36 @@
 const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
+const APP_DIR = path.join(__dirname, 'src');
+const APP_FILE_NAME = "app.js";
+const APP_HTML_TEMPLEATE = "index.html";
+const APP_HTML_FILENAME = "index.html";
 const OUTPUT_PATH = path.resolve(__dirname, './dist');
 const BUNDLE_FILENAME = 'app.bundle.js';
 
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: path.join(APP_DIR, APP_HTML_TEMPLEATE),
+  filename: "index.html"
+});
+
 module.exports = {
-  entry: './src/app.js',
+  entry: path.join(APP_DIR, APP_FILE_NAME),
   output: {
     path: OUTPUT_PATH,
     filename: BUNDLE_FILENAME
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          babelrc: false,
+          presets: ["babel-preset-env", "stage-0"]
+        }
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
 }
